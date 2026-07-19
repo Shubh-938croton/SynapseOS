28cd5d4faedca7413e17ad50dad3cdcb165b15e5
Shubh kamal <crotonaldehyde938@gmail.com>
2026-07-19 13:46:27 +0530

    Dynamic task adder.

--- frontend/JS/script.js
+++ frontend/JS/script.js
@@ -1,3 +1,6 @@
+// for the theme change 
+
+
 const themeButton = document.getElementById("theme-toggle");
 themeButton.addEventListener("click", function () {
   document.body.classList.toggle("dark-mode");
@@ -6,4 +9,16 @@ themeButton.addEventListener("click", function () {
   } else {
     themeButton.textContent = "🌙 Dark Mode";
   }
+});
+
+
+// for the taking task 
+
+const taskInput=document.getElementById("taskInput");
+const addTaskBtn=document.getElementById("addTaskBtn");
+const taskList=document.getElementById("taskList");
+
+
+addTaskBtn.addEventListener("click",function(){
+    alert("Button Clicked:");
 });
