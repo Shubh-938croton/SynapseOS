6d0e2c247715f2411766db1468762ae31139f937
Shubh kamal <crotonaldehyde938@gmail.com>
2026-07-26 14:21:42 +0530

    Added: notes table in database.

---
+++ database/schema/v1/005_notes.sql
@@ -0,0 +1,22 @@
+USE synapseos;
+
+CREATE TABLE notes (
+    node_id INT AUTO INCREMENT PRIMARY KEY ,
+    user_id INT NOT NULL,
+    subject_id INT NOT NULL,
+    title VARCHAR(200) NOT NULL,
+
+    content TEXT NOT NULL,
+
+    is_pinned BOOLEAN DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    ON UPDATE CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (user_id)
+        REFERENCES users(user_id)
+        ON DELETE CASCADE ,
+
+    FOREIGN KEY (subject_id)
+        REFERENCES subjects(subject_id)
+        ON DELETE CASCADE
+);
