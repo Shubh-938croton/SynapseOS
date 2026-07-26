f9d6850e6e8c8fcc9057a8f3682a2c462e81fa86
Shubh kamal <crotonaldehyde938@gmail.com>
2026-07-26 11:05:05 +0530

    Updated the folder structure of the project.

--- database/seed.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO users(name,email,password_hash)
-VALUES 
-('shubh','shubhkamal@gmail.com','1234')
- INSERT INTO subjects(user_id,subject_name)
- VALUES 
- (1,'DSA'),
- (1,'DBMS'),
- (1,'Web Development');
