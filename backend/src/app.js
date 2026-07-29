856766abd8fb015eb3fd70e49fd8e9f41bfc0614
Shubh kamal <crotonaldehyde938@gmail.com>
2026-07-29 09:39:05 +0530

    Set up Express server and application structure

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,2 +1,13 @@
-const express =require("express");
+const express = require("express");
 
+const app = express();
+
+app.get("/", function (req, res) {
+    res.json({
+        project: "SynapseOS",
+        version: "1.0",
+        status: "Backend Running"
+    });
+});
+
+module.exports = app;
