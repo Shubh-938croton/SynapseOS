583baef3cc92cd105dbce348e6f10d9667f3f200
Shubh kamal <crotonaldehyde938@gmail.com>
2026-08-03 22:09:12 +0530

    Initialize React frontend with Vite and project architecture

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,37 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+import Dashboard from "../pages/Dashboard/Dashboard";
+
+function AppRoutes() {
+    return (
+        <BrowserRouter>
+            <Routes>
+
+                <Route
+                    path="/"
+                    element={<Login />}
+                />
+
+                <Route
+                    path="/login"
+                    element={<Login />}
+                />
+
+                <Route
+                    path="/register"
+                    element={<Register />}
+                />
+
+                <Route
+                    path="/dashboard"
+                    element={<Dashboard />}
+                />
+
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default AppRoutes;
