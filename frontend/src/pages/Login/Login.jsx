3eda51a0b6b72144eaf5be072801e830e9cd8bb4
Shubh kamal <crotonaldehyde938@gmail.com>
2026-08-07 22:12:15 +0530

    login page design started

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { loginUser } from "../../services/authService";
+import "./Login.css";
 
 function Login() {
 
